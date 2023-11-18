a20bfc2b64e34a8d96251ea8b4aa6836621d749b
indigo165e83 <indigo165e833@gmail.com>
2023-11-18 22:00:43 +0900

    ヘッダーのオーナー名を変更

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <>
       <h1 className="text-4xl text-center p-2">
-        ポートフォリオ@aibizsasaki
+        ポートフォリオ@indigo
       </h1>
       <p className="text-2xl text-center text-gray-600 p-2">
         Thank you for visiting my site!
